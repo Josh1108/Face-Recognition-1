093abb998863c699d241fb14af6c46ee7b7bad00
Anuj Verma <ashes4799@gmail.com>
2020-06-06 22:33:23 +0530

    Navbar Fixed

--- databaseUI/config.py
+++ databaseUI/config.py
@@ -6,7 +6,7 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    FLASK_ADMIN_SWATCH = 'cerulean'
+    FLASK_ADMIN_SWATCH = 'superhero'
     UPLOAD_IMAGES_DEST ='static/images'
     UPLOAD_IMAGES_URL = '../static/images/'
     DEBUG = True
